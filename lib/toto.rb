1b826beb2f7f05782e575d89a171691b20e806ba
Doug McInnes <doug@dougmcinnes.com>
2016-05-02 22:10:28 -0700

    support for HEAD requests

--- lib/toto.rb
+++ lib/toto.rb
@@ -330,14 +330,14 @@ module Toto
       @request  = Rack::Request.new env
       @response = Rack::Response.new
 
-      return [400, {}, []] unless @request.get?
+      return [400, {}, []] unless @request.get? || @request.head?
 
       path, mime = @request.path_info.split('.')
       route = (path || '/').split('/').reject {|i| i.empty? }
 
       response = @site.go(route, env, *(mime ? mime : []))
 
-      @response.body = [response[:body]]
+      @response.body = [response[:body]] unless @request.head?
       @response['Content-Length'] = response[:body].bytesize.to_s unless response[:body].empty?
       @response['Content-Type'] = Rack::Mime.mime_type(".#{response[:type]}")
 
